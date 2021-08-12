4680d623049b3ef7f375873c82af8bc32236e638
DMITRY LOKHMATOV <maloll777@gmail.com>
2021-08-12 22:59:57 +0300

    расписал примерный функционал с БД

--- warehose_DB.py
+++ warehose_DB.py
@@ -12,17 +12,46 @@ def db_connect(func, *args) -> object:
     )
     cursor = connect.cursor()
 
-    def wrapper(*args):
-        return func(cursor, *args)
+    def wrapper(*arg):
+        return func(cursor, *arg)
 
     return wrapper
 
 
 @db_connect
 def find_code_product(cursor, id_product):
-    # ищет по id товара информацию о нем
+    # по id товара информацию о нем
 
     cursor.execute("select code_product, product_name, subgroup_product, group_product from product where "
                    "code_product = '{}';".format(id_product))
     return cursor.fetchall()
 
+@db_connect
+def add_product_whouse():
+    # добавляет товар на склад
+    pass
+
+@db_connect
+def remove_product_whouse():
+    # удаляет товар со склада
+    pass
+
+@db_connect
+def move_product_warehouse():
+    # создает перемещение между складами
+    pass
+
+@db_connect
+def write_off_product():
+    # списание товара со склада
+    pass
+
+@db_connect
+def shipment_product():
+    # отгрузка товара клиенту со склада
+    pass
+
+@db_connect
+def cancel_shipment():
+    # возврат товара от клиента
+    pass
