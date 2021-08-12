3cd286867cf66c255c07a508594e8ce5b2110e47
DMITRY LOKHMATOV <maloll777@gmail.com>
2021-08-12 22:39:07 +0300

    добовил в warehouse_DB функцию поиска по id товара

--- warehose_DB.py
+++ warehose_DB.py
@@ -1,7 +1,7 @@
 import psycopg2
 
 
-def db_connect(func):
+def db_connect(func, *args) -> object:
     # декоратор для подключения к БД
     connect = psycopg2.connect(
         database="triol",
@@ -10,8 +10,19 @@ def db_connect(func):
         host="127.0.0.1",
         port="5432"
     )
+    cursor = connect.cursor()
 
-    def wrapper():
-        return func(connect)
+    def wrapper(*args):
+        return func(cursor, *args)
 
     return wrapper
+
+
+@db_connect
+def find_code_product(cursor, id_product):
+    # ищет по id товара информацию о нем
+
+    cursor.execute("select code_product, product_name, subgroup_product, group_product from product where "
+                   "code_product = '{}';".format(id_product))
+    return cursor.fetchall()
+
